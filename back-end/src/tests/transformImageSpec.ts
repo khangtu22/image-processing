2bb3bd0586b5ab941ff4d8260653c9e11bc7f6b6
khangtv <vankhang226@gmail.com>
2023-09-10 10:50:38 +0700

    Add test transform

--- back-end/src/tests/transformImageSpec.ts
+++ back-end/src/tests/transformImageSpec.ts
@@ -1,7 +1,7 @@
 import request from "supertest"
 import app from "../app"
 
-describe("validateImageParams", () => {
+describe("Transform Image", () => {
   it("should return 400 if imageId is missing", async () => {
     const response = await request(app).get("/api/image?width=100&height=150")
     expect(response.status).toBe(400)
@@ -18,5 +18,12 @@ describe("validateImageParams", () => {
     const response = await request(app).get("/api/image?imageId=someId&width=100&height=-5")
     expect(response.status).toBe(400)
     expect(response.text).toContain('Invalid "height" value: -5')
+  })
+
+  it("should set res.locals.imageId, res.locals.width, and res.locals.height and call next() for valid params", async () => {
+    const response = await request(app).get(
+      "/api/image?imageId=encenadaport.jpg&width=100&height=150"
+    )
+    expect(response.status).toBe(200) // Assuming your middleware calls next() and returns 200
   })
 })
